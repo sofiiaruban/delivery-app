2ffc17e40c71f11fe2dd3bec456bda14fa07ea27
Sofia Ruban <sophiaruban249324@gmail.com>
2023-05-23 19:28:53 +0300

    refactor(navbar): Add link instead of a

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,14 @@
+import {Link } from 'react-router-dom';
+
 const NavBar: React.FC = () => {
     return ( 
     <ul>
-        <li><a href="/shop">Shop</a></li>
-        <li><a href="/cart">Shopping Cart</a></li>
+        <li>
+            <Link to="/shop">Shop</Link>
+        </li>
+        <li>
+            <Link to="/cart">Shopping Cart</Link>
+        </li>
     </ul>)
  }
  export default NavBar
