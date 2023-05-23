4efc1a8a9a3013e36e7b3838756d9e0d413160e7
Sofia Ruban <sophiaruban249324@gmail.com>
2023-05-23 20:11:52 +0300

    feat(approuter): Add fetch data func

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useQuery } from "react-query";
 import Shop from "./pages/Shop";
 import ShoppingCart from "./pages/ShoppingCart";
+
+const fetchData = async (url: string) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
 
 const AppRouter: React.FC = () => {
   return (
